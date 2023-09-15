d9b8dc4e79b276fca8d191e3901df3de8b933e73
isidrei <musni.danisidrei@auf.edu.ph>
2023-09-15 11:37:34 +0800

    first commit

---
+++ MVVM/MVVM/ViewModel/TaskViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MVVM.Model;
+
+namespace MVVM.ViewModel
+{
+    class TaskViewModel
+    {
+        public TaskModel TaskModelSet { get; set; }
+
+        public TaskViewModel()
+        {
+            TaskModelSet = new TaskModel
+            {
+                SubjectCode ="PDC06",
+                SubjectTitle ="Xamarin Mobile Programming",
+                Unit =3,    
+
+            };
+        }
+    }
+}
